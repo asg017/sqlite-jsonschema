de61d113dc3cee2407852d27039cb24309bce5c5
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-02-21 22:37:41 -0800

    windows and mac fix

--- npm/sqlite-jsonschema/src/index.js
+++ npm/sqlite-jsonschema/src/index.js
@@ -9,6 +9,7 @@ const supportedPlatforms = [
   ["win32", "x64"],
   ["linux", "x64"],
 ];
+
 function validPlatform(platform, arch) {
   return (
     supportedPlatforms.find(([p, a]) => platform == p && arch === a) !== null
@@ -18,6 +19,10 @@ function extensionSuffix(platform) {
   if (platform === "win32") return "dll";
   if (platform === "darwin") return "dylib";
   return "so";
+}
+function platformPackageName(platform, arch) {
+  const os = platform === "win32" ? "windows" : platform;
+  return `sqlite-jsonschema-${ps}-${arch}`;
 }
 
 export function getLoadablePath() {
@@ -30,17 +35,18 @@ export function getLoadablePath() {
         )}). Consult the sqlite-jsonschema NPM package README for details. `
     );
   }
+  const packageName = platformPackageName(platform, arch);
   const loadablePath = join(
     fileURLToPath(new URL(".", import.meta.url)),
     "..",
     "..",
-    `sqlite-jsonschema-${platform}-${arch}`,
+    packageName,
     "lib",
     `jsonschema0.${extensionSuffix(platform)}`
   );
   if (!statSync(loadablePath, { throwIfNoEntry: false })) {
     throw new Error(
-      `Loadble extension for sqlite-jsonschema not found. Was the sqlite-jsonschema-${platform}-${arch} package installed? Avoid using the --no-optional flag, as the optional dependencies for sqlite-jsonschema are required.`
+      `Loadble extension for sqlite-jsonschema not found. Was the ${packageName} package installed? Avoid using the --no-optional flag, as the optional dependencies for sqlite-jsonschema are required.`
     );
   }
 
